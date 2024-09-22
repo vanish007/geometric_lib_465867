1cb84921cc45d4557978f8242f8ee092453752b0
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-09-22 23:23:08 +0300

    added documentation of an area function

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,14 @@
 def area(a, b):
+    '''
+    Returns the area of a rectangle with 2 given sides of a and b
+
+    Parameters:
+        a: (int) - first side of a rectangle
+        b: (int) - second side of a rectangle
+    
+    Return values:
+        area(a, b): (int) - area of a rectangle
+    '''
     return a * b
 
 def perimeter(a, b):
