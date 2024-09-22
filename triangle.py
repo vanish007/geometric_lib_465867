41a780bf5509d1a60ea561d5ca0791d3fc52480c
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-09-22 23:43:34 +0300

    added documentation to the functions in the triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,26 @@
 def area(a, h):
+    '''
+    Returns the area of a triangle with given base and height
+
+    Parameters:
+        a: (float) - base of a rectangle
+        h: (float) - height of a rectangle
+    
+    Return values:
+        area(a, h): (float) - area of a rectangle
+    '''
     return a * h / 2
 
 def perimeter(a, b, c):
+    '''
+    Returns the perimeter of a triangle with 3 given sides
+
+    Parameters:
+        a: (float) - first side of a rectangle
+        b: (float) - second side of a rectangle
+        c: (float) - third side of a rectangle
+    
+    Return values:
+        perimeter(a, b, c): (float) - perimeter of a rectangle
+    '''
     return a + b + c
