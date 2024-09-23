4a92799bedbffb3e30665323e31f606427b842f7
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-09-23 10:18:30 +0300

    added describtion of the functions in the circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,27 @@ import math
 
 
 def area(r):
+    '''
+    Return the area of a circle with radius r
+    
+    Parametrs:
+        r: (float) - radius of a circle
+    
+    Return values:
+        area(r): (float) - area of a circle
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Return the perimeter of a circle with radius r
+    
+    Parametrs:
+        r: (float) - perimeter of a circle
+    
+    Return values:
+        perimeter(r): (float) - perimeter of a circle
+    '''
     return 2 * math.pi * r
 
