4c02d57a98549d82e7aea4c48364002f4d7a9bcc
Ivan Erofeev <ivanerofeev01@gmail.com>
2024-09-23 10:20:20 +0300

    added describtion of the functions in the square.py

--- square.py
+++ square.py
@@ -1,7 +1,25 @@
 
 def area(a):
+    '''
+    Returns the area of a square with given side a
+
+    Parametrs:
+        a: (float) - side of a square
+    
+    Return values:
+        area(a): float - area of a square
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Returns the perimeter of a square with given side a
+
+    Parametrs:
+        a: (float) - side of a square
+    
+    Return values:
+        perimeter(a): float - area of a square
+    '''
     return 4 * a
